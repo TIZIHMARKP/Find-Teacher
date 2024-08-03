03967ffbc50ca7a20bbb4ca938f6653d1d371484
unknown <tizihmarkprincewil@gmail.com>
2024-08-03 21:28:04 +0100

    Done with teacher landing page

---
+++ l-page.js
@@ -0,0 +1,7 @@
+
+window.onload = function(){
+    // Remove the hidden class from the elements to start the animations
+    document.querySelector('.falling-text').classList.remove('hidden');
+    document.querySelector('.sliding-text').classList.remove('hidden');
+
+}
